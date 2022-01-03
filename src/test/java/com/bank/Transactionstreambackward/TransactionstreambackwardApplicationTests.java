e2524a1dab4fe97891425532214f0d0d21f8811b
Anoop <Anoop@NTP-LAP-226.chn.nestgroup.net>
2022-01-03 17:14:13 +0530

    This stream for publish for read the bank appi putpose

---
+++ src/test/java/com/bank/Transactionstreambackward/TransactionstreambackwardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.Transactionstreambackward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionstreambackwardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
